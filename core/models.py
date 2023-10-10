16dc226f84349ef98f80454ede60b010548116ec
Steve-Dusty <kuantingk2@gmail.com>
2023-10-09 22:03:43 -0700

    created database user relationships

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+class User(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    school = models.CharField(max_length=255)
+    address = models.TextField()
+    is_driver = models.BooleanField()
+
+class Post(models.Model):
+    frequency = models.CharField(max_length=255)
+    time = models.TimeField()
+    date = models.DateField()
+    additional_info = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user
+
+class Pair(models.Model):
+    driver = models.OneToOneField(User, on_delete=models.CASCADE, related_name="driver")
+    rider = models.OneToOneField(User, on_delete=models.CASCADE, related_name="rider")
