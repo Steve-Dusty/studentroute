1d43cd8b8b17674ac626fb5bb19815f87dc54578
Steve-Dusty <kuantingk2@gmail.com>
2023-10-10 21:55:09 -0700

    added password hashing in serializer

--- core/serializers.py
+++ core/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
 User = get_user_model()
 
@@ -16,3 +17,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super(UserSerializer, self).create(validated_data)
