0aaab0acd6c9796ac61ba30bb3f4087b5e12f520
Steve-Dusty <kuantingk2@gmail.com>
2023-10-14 10:55:56 -0700

    moved order of cors middlewhere. clear browsing data (cache to fix cors issue, nothing wrong with code

--- studentroute/settings.py
+++ studentroute/settings.py
@@ -53,6 +53,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
